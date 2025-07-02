9e36d56f152b8159ed2ea648208ad00387b42223
nalina <naleenameshram85@gmail.com>
2025-07-02 23:08:05 +0530

    working

--- Calendar/src/App.jsx
+++ Calendar/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   const [dayinMonth, setDayinMoth] = useState([]);
   const [startDay, setStartDay] = useState(0);
   const [selectedDate, setSelectedDate] = useState(null);
-  console.log(currentDate);
-  console.log(dayinMonth, selectedDate, startDay);
+  // console.log(currentDate);
+  // console.log(dayinMonth, selectedDate, startDay);
 
   useEffect(() => {
     const year = currentDate.getFullYear();
@@ -16,16 +16,62 @@ function App() {
     const days = [];
     while (date.getMonth() === month) {
       days.push(new Date(date));
-      date.setDate(date.getDate() - 1);
+      date.setDate(date.getDate() + 1);
     }
     setDayinMoth(days);
-    setCurrentDate(new Date(year, month, 1).getDay());
+    setStartDay(new Date(year, month, 1).getDay());
   }, [currentDate]);
 
-  const dayMonth = ["MON", "Tue", "Web", "Thu", "Fri", "Sat", "Sun"];
+  const dayMonth = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+
+  const prevMonth = () => {
+    setCurrentDate(new Date(currentDate.setMonth(currentDate.getMonth() - 1)));
+  };
+
+  const nextMonth = () => {
+    setCurrentDate(new Date(currentDate.setMonth(currentDate.getMonth() + 1)));
+  };
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
   return (
     <div className="calender">
-      <div className="header">&lt;</div>
+      <div className="header">
+        <button onClick={prevMonth}>&lt;</button>
+        <span>
+          {currentDate.toLocaleString("default", { month: "long" })}
+          {currentDate.getFullYear()}
+        </span>
+        <button onClick={nextMonth}>&gt;</button>
+      </div>
+      <div className="days-names">
+        {dayMonth.map((day) => (
+          <div key={day}>{day}</div>
+        ))}
+      </div>
+      <div className="days">
+        {Array.from({ length: startDay }).map((_, index) => (
+          <div key={index} className="empty-day"></div>
+        ))}
+        {dayinMonth.map((day) => (
+          <div
+            key={day}
+            className={`day ${
+              day.getDate() === new Date().getDate() &&
+              day.getMonth() === new Date().getDate()
+                ? "today"
+                : ""
+            } ${
+              selectedDate && day.toDateString() === selectedDate.toDateString()
+                ? "selected"
+                : ""
+            }`}
+            onClick={() => handleDateClick(day)}
+          >
+            {day.getDate()}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
